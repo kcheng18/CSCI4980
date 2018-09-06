b47188429cecd105956ec24d6633882348ae18db
Kwok Sun Cheng <kwoksuncheng@unomaha.edu>
2018-09-06 17:23:05 -0500

    adding tableview Ex

---
+++ simpleTable1Example/src/model/PersonModelProvider.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum PersonModelProvider {
+   INSTANCE;
+	//INSTANCE(getfilepath);
+
+   private List<Person> persons;
+
+   private PersonModelProvider() {
+      persons = new ArrayList<Person>();
+      persons.add(new Person("FirstNameA", "LastNameA", "female", true));
+      persons.add(new Person("FirstNameB", "LastNameB", "female", true));
+      persons.add(new Person("FirstNameC", "LastNameC", "female", true));
+      persons.add(new Person("FirstNameD", "LastNameD", "male", true));
+      persons.add(new Person("FirstNameE", "LastNameE", "male", false));
+   }
+
+   public List<Person> getPersons() {
+      return persons;
+   }
+}
