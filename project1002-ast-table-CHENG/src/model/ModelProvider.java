59dbb12b0187975f949784b4c1265e4e72e65865
Kwok Sun Cheng <kwoksuncheng@unomaha.edu>
2018-10-02 17:20:57 -0500

    adding project1002

---
+++ project1002-ast-table-CHENG/src/model/ModelProvider.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum ModelProvider {
+   INSTANCE;
+
+   private List<ProgramElement> progElements = new ArrayList<ProgramElement>();;
+
+   private ModelProvider() {
+   }
+
+   public void addProgramElements(String pkgName, String className, String methodName, boolean isRetVoid, int parmSize, int start) {
+      progElements.add(new ProgramElement(pkgName, className, methodName, isRetVoid, parmSize, start));
+   }
+
+   public List<ProgramElement> getProgramElements() {
+      return progElements;
+   }
+
+   public void clearProgramElements() {
+      progElements.clear();
+   }
+}
